67c27cd86b94b5f97dbba8fcb3818dd207bc37e5
praveen kumar <praveen4554@gmail.com>
2018-02-06 23:35:51 +0530

    component life cycle hooks

--- Angular5/src/app/app.module.ts
+++ Angular5/src/app/app.module.ts
@@ -8,23 +8,23 @@ import { Router, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ChildComponentComponent } from './child-component/child-component.component';
+import { HomeChildComponent } from './home-child/home-child.component';
+import { routing } from './class.routes';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ChildComponentComponent,
+    HomeChildComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {path:'Home',component:HeaderComponent},
-      {path:'Footer',component:FooterComponent},
-      {path:'logOut',redirectTo:'Footer',pathMatch:'full'},
-      {path:'**',component:HeaderComponent}
-    ])
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
